52a75a8262814b22dcc81444c2b65506d93ed788
santiagoRojas <santi_rojas123@hotmail.com>
2020-08-27 10:20:32 -0500

    pagina
    falta todo

---
+++ Laboratorios/web/Vue.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    tabla: ['Noticias','Software', 'Laboratorios']
+  }
+})
